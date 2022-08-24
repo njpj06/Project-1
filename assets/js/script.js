0aa49f40243ce82678b09954c1f1822d5eeff5e2
G Birge <gabriellebirge@gmail.com>
2022-08-23 21:16:16 -0400

    fixing css link, added js variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var word = document.getElementById("word")
+var definition = document.getElementById("definition")
+var wiki = document.getElementById("wiki")
+var btn = document.getElementById("btn")
