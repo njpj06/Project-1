69034e019ca5a46d3f641ca2826a852d2bc259ef
G Birge <gabriellebirge@gmail.com>
2022-08-29 21:31:42 -0400

    font change to definition tag, linking an object response to dictionary api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var word = document.getElementById("word");
 var definition = document.getElementById("definition");
-var wiki = document.getElementById("wiki");
 var btn = document.getElementById("btn");
 
 const wordOptions = {
@@ -20,7 +19,8 @@ fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", wordOptions)
     .then((response) => {
         console.log(response.word);
             fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${response.word}?key=ba8ba0ae-582d-43a4-8472-0c05e1fbde80`, dictionaryOptions)
-            .then((response) => response.json())
+            .then((response) => response.json()
+            .then(definition.innerText = response))
             .then((response) => console.log(response))
             word.innerText = response.word;
             })
