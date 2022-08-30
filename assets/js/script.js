c50aecc7c084bc09303fd887dc42f7e51e3d75cb
G Birge <gabriellebirge@gmail.com>
2022-08-30 18:39:33 -0400

    connected dictionary API output to display on front end

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,11 @@ fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", wordOptions)
         console.log(response.word);
             fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${response.word}?key=ba8ba0ae-582d-43a4-8472-0c05e1fbde80`, dictionaryOptions)
             .then((response) => response.json()
-            .then(definition.innerText = response))
-            .then((response) => console.log(response))
+            .then((response) => {
+            console.log(response);
+            const data = response;
+            definition.innerText = data;
+            })),
             word.innerText = response.word;
             })
             
